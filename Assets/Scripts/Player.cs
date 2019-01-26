a47757bea515773d5a0339ca9db23054b82463ad
iHenrik <ismo.loukusa@gmail.com>
2019-01-26 09:22:46 +0200

    Add initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject laser;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        FollowMouse();
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Instantiate(laser, transform.position, transform.rotation);
+        }
+    }
+
+    private void FollowMouse()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(
+            new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(Camera.main.transform.position.z - transform.position.z)));
+
+        transform.position = mousePosition;
+    }
+}
